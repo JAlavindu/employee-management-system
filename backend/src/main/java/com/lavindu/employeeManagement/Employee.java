9331d8a5de24abbc8a83a67289b376ebf5a7dcc0
lavindu perera <lavi156perera@gmail.com>
2025-12-04 13:47:54 +0530

    created employee class

---
+++ backend/src/main/java/com/lavindu/employeeManagement/Employee.java
@@ -0,0 +1,32 @@
+package com.lavindu.employeeManagement;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID empCode;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String NIC;
+    private String mobileNo;
+    private String gender;
+    private String email;
+    private String designation;
+    private String status;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+}
