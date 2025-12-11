2189ef433413a42b7193cd10cff61e9636402f8f
lavindu perera <lavi156perera@gmail.com>
2025-12-11 15:58:47 +0530

    fixed errors with interceptors

--- frontend/src/core/interceptors/auth.interceptor.ts
+++ frontend/src/core/interceptors/auth.interceptor.ts
@@ -1,15 +1,12 @@
-import { Injectable } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('token');
-  let modifiedReq = req;
+  const token = localStorage.getItem('authToken');
 
   if (token) {
-    modifiedReq = req.clone({
+    const modifiedReq = req.clone({
       setHeaders: {
-        Authorization: `Bearers ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     });
     return next(modifiedReq);
